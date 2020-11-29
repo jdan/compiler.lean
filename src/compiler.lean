0dcffe8c3e2d63fab2cfe195f238d4d458f56863
Jordan Scales <scalesjordan@gmail.com>
2020-11-29 11:55:27 -0500

    tag some functions with simp
    thanks https://leanprover.zulipchat.com/#narrow/stream/113489-new-members/topic/.22Program.20Correctness.22.20on.20Computerphile/near/218216018

--- src/compiler.lean
+++ src/compiler.lean
@@ -7,8 +7,8 @@ inductive expr
 | val : ℕ -> expr
 | add : expr -> expr -> expr
 
-/- evaluate an expression directly -/
-def eval : expr -> ℕ
+/-- evaluate an expression directly -/
+@[simp] def eval : expr -> ℕ
 | (expr.val n) := n
 | (expr.add a b) := eval a + eval b
 
@@ -20,48 +20,39 @@ inductive instr
 | push : ℕ -> instr
 | add : instr
 
-/- execute a list of instructions on a stack -/
-def exec : list instr -> list ℕ -> list ℕ
+/-- execute a list of instructions on a stack -/
+@[simp] def exec : list instr -> list ℕ -> list ℕ
 | ((instr.push n) :: rest) s :=
   exec rest (n :: s)
 | (instr.add :: rest) (a :: b :: s) :=
   exec rest ((a + b) :: s)
 | _ s := s
 
-/- compile an expression to an instruction list -/
-def compile : expr -> list instr
+/-- compile an expression to an instruction list -/
+@[simp] def compile : expr -> list instr
 | (expr.val n) := [instr.push n]
 | (expr.add a b) := compile a ++ compile b ++ [instr.add]
 
-/- compiled expressions only add to the stack when `exec`d -/
+/-- compiled expressions only add to the stack when `exec`d -/
 lemma exec_compile_concat : ∀ e instrs stack,
   exec (compile e ++ instrs) stack = exec instrs (eval e :: stack) :=
 begin
   intro e,
   induction e with n a b iha ihb,
-  { unfold compile, unfold eval,
-    intros,
+  { intros,
     simp,
-    unfold exec,
   },
-  { unfold compile, unfold eval,
-    intros,
-    simp,
-    repeat { rw [iha, ihb] },
-    unfold exec,
-    rw add_comm,
+  { intros,
+    simp [iha, ihb, add_comm],
   }
 end
 
-/- exec (compile e) = eval e -/
+/-- exec (compile e) = eval e -/
 theorem eval_eq_exec_compile : ∀ e,
   exec (compile e) [] = [eval e] :=
 begin
   intros,
-  have H := exec_compile_concat e [] [],
-  simp at H,
-  rw H,
-  unfold exec,
+  simpa using exec_compile_concat e [] [],
 end
 
 end compiler
