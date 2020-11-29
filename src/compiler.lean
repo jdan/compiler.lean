6814d9031a0c4ceae2919ca3619dbcf41690ba99
Jordan Scales <scalesjordan@gmail.com>
2020-11-29 12:27:38 -0500

    rearrange compile and exec

--- src/compiler.lean
+++ src/compiler.lean
@@ -22,16 +22,16 @@ inductive Instr
 | ADD : Instr
 open Instr
 
+/-- compile an expression to an instruction list -/
+@[simp] def compile : Expr -> list Instr
+| (Val n) := [PUSH n]
+| (Add a b) := compile a ++ compile b ++ [ADD]
+
 /-- execute a list of instructions on a stack -/
 @[simp] def exec : list Instr -> list ℕ -> list ℕ
 | ((PUSH n) :: rest) s := exec rest (n :: s)
 | (ADD :: rest) (a :: b :: s) := exec rest ((a + b) :: s)
 | _ s := s
-
-/-- compile an expression to an instruction list -/
-@[simp] def compile : Expr -> list Instr
-| (Val n) := [PUSH n]
-| (Add a b) := compile a ++ compile b ++ [ADD]
 
 /-- compiled expressions only add to the stack when `exec`d -/
 @[simp] lemma exec_compile_concat (e : Expr) : ∀ instrs stack,
